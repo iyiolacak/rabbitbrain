b2c61ec0ed867180b36fccf6855c0b9415e4c327
iyiolacak <iyiolacak0@gmail.com>
2024-09-07 17:27:09 +0300

    "gimport" typo fix

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-gimport { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
