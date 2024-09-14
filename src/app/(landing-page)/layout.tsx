528f062d54e0ce27049c25110bb2907ddcf44a3e
iyiolacak <iyiolacak0@gmail.com>
2024-09-14 22:06:45 +0300

    Hero section added back. Switched 'Infinite Carousel' to dark mode and moved forward with production.

--- src/app/(landing-page)/layout.tsx
+++ src/app/(landing-page)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./_components/Navbar";
 
 const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col w-full">
+    <div className="bg-black/90 flex flex-col w-full">
       <Navbar />
       {children}
     </div>
--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -13,27 +13,74 @@ import {
   Flask,
   Lock,
   BrainElectricity,
+  EmojiPuzzled,
+  Group,
+  CodeBrackets,
 } from "iconoir-react";
 import Hero from "./_components/Hero";
 import { motion } from "framer-motion";
 
 const conceptsList = [
-  { name: "Brain Teasers", description: "Fun Puzzles to Sharpen Your Mind", icon: Brain, color: "#F59E0B" }, // Orange
-  { name: "Math", description: "Tackle Numbers with Confidence", icon: SigmaFunction, color: "#10B981" }, // Green
-  { name: "Science", description: "Explore the World, One Concept at a Time", icon: Atom, color: "#3B82F6" }, // Blue
-  { name: "Programming & AI", description: "Build your first smart AI—Step by Step", icon: Code, color: "#6366F1" }, // Indigo
-  { name: "Data Science", description: "Turn Data into Simple Insights", icon: DatabaseStats, color: "#EF4444" }, // Red
-  { name: "Cybersecurity", description: "Keep Systems Safe in a Fun Way", icon: ShieldCheck, color: "#06B6D4" }, // Cyan
-  { name: "Chemistry", description: "Understand Reactions", icon: Flask, color: "#A855F7" }, // Purple
-  { name: "Cryptography", description: "Unlock the Secrets of Secure Tech", icon: Lock, color: "#F97316" }, // Orange
-  { name: "Psychology & Behavior", description: "Learn What Makes Us Tick", icon: BrainElectricity, color: "#F43F5E" }, // Pink
+  {
+    name: "Brain Teasers",
+    description: "Fun Puzzles to Sharpen Your Mind",
+    icon: Brain,
+    color: "#f497da", // Deep Amber
+  },
+  {
+    name: "Math",
+    description: "Tackle Numbers with Confidence",
+    icon: SigmaFunction,
+    color: "#6f9ceb", // Teal Green
+  },
+  {
+    name: "Science",
+    description: "Explore the World, One Concept at a Time",
+    icon: Atom,
+    color: "#007fff", // Deep Blue
+  },
+  {
+    name: "Programming & AI",
+    description: "Build your first smart AI—Step by Step",
+    icon: CodeBrackets,
+    color: "#6a0f49", // Royal Indigo
+  },
+  {
+    name: "Data Science",
+    description: "Turn Data into Simple Insights",
+    icon: DatabaseStats,
+    color: "#DC2626", // Crimson Red
+  },
+  {
+    name: "Cybersecurity",
+    description: "Keep Systems Safe in a Fun Way",
+    icon: ShieldCheck,
+    color: "#0891B2", // Dark Cyan
+  },
+  {
+    name: "Chemistry",
+    description: "Understand Reactions",
+    icon: Flask,
+    color: "#6e44ff", // Vivid Violet
+  },
+  {
+    name: "Cryptography",
+    description: "Unlock the Secrets of Secure Tech",
+    icon: Lock,
+    color: "#f4e409", // Burnt Orange
+  },
+  {
+    name: "Psychology",
+    description: "Learn What Makes Us Tick",
+    icon: Group,
+    color: "#c3423f", // Ruby Wine
+  },
 ];
 const LandingPage = () => {
   const router = useRouter();
   const handleSignUpCTA = () => router.push("/sign-up");
   const [hoverIndex, setHoverIndex] = useState(null);
 
-  // const conceptsList: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [index, setIndex] = useState(4);
   const [replacementIndex, setReplacementIndex] = useState(0);
   const [conceptsDisplay, setConceptsDisplay] = useState<typeof conceptsList>([
@@ -43,91 +90,107 @@ const LandingPage = () => {
     conceptsList[3],
   ]);
 
-  const handleNextReplacement = () => {
-    function positiveMod(n: number, m: number): number {
-      return ((n + m) % m) % m;
-    }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const handleNextReplacement = () => {
+        // if `n` is negative, it could cause an array index issue.
+        function positiveMod(n: number, m: number): number {
+          return (n + m) % m;
+        }
 
-    const nextIndexToReplace = positiveMod(replacementIndex - 4, conceptsDisplay.length);
-    // -4 % 4 = 0
-    // 0 % 4 = 0
-    
-    setConceptsDisplay((prevDisplay) => {
+        const nextIndexToReplace = positiveMod(
+          replacementIndex,
+          conceptsDisplay.length
+        );
 
-      const newDisplay = [...prevDisplay];
-      newDisplay[nextIndexToReplace] = conceptsList[index % conceptsList.length];
-      // Return the new array, causing React to update the state
-      return newDisplay;
-    });
-    setReplacementIndex(prev => prev + 1)
-    setIndex((prevIndex) => (prevIndex + 1) % conceptsList.length);
-  };
+        setConceptsDisplay((prevDisplay) => {
+          const newDisplay = [...prevDisplay];
+          newDisplay[nextIndexToReplace] =
+            conceptsList[index % conceptsList.length];
+          // Return the new array, causing React to update the state
+          return newDisplay;
+        });
+        setReplacementIndex((prev) => prev + 1);
+        setIndex((prevIndex) => (prevIndex + 1) % conceptsList.length);
+      };
+      handleNextReplacement();
+    }, 500);
+    return () => clearInterval(interval);
+  }, [conceptsDisplay, index, replacementIndex]);
 
   // Stop updating once we cycle through the full conceptsList
   console.log(conceptsDisplay);
-  return (
-    <div className="min-h-screen bg-black">
-      {/* <Hero /> */}
 
-      <section className="bg-white py-6 px-6 md:px-6">
-        <div className="w-full flex justify-center space-x-10 mb-16">
-          <p className="font-bold text-zinc-400 mr-20">{index}</p>
-          {/* {conceptsDisplay.map((item, idx) => (
-            <span key={idx} style={{ marginRight: "10px" }}>
-              {item}
-            </span>
-          ))} */}
-          <Button
-            onClick={handleNextReplacement}
-            disabled={index >= conceptsList.length}
-          >
-            Next Replacement
-          </Button>
+  return (
+    <div className="w-full min-h-screen bg-black">
+      <Hero />
+      <section className="w-full py-3 md:py-6 px-6 md:px-8 bg-black">
+        <div className="w-full flex justify-center md:space-x-10 mb-3 md:mb-16">
+          {/* Desktop Version: Display all elements */}
           {conceptsDisplay.map(({ name, icon: Icon, color }) => (
             <motion.div
-            className="min-w-max p-4 flex flex-col items-center group cursor-pointer"
-            key={name}
+              className="hidden w-72 h-16 md:flex flex-col items-center group cursor-pointer"
+              key={name}
             >
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center justify-center space-x-1">
                 <span>
-                  <Icon width={36} height={36} color={color} />
+                  <Icon width={48} height={48} color={color} />
                 </span>
-                <h3 className="font-serif text-3xl text-zinc-800 font-medium">
+                <h3 className="font-serif text-3xl text-white truncate font-medium ">
                   {name}
                 </h3>
               </div>
             </motion.div>
           ))}
-          {/* Show the updated array on the screen */}
+          {/* Mobile Version: Display only the first element */}
+          {conceptsDisplay.slice(0, 1).map(({ name, icon: Icon, color }) => (
+            <motion.div
+              className="md:hidden w-full h-16 flex flex-col items-center justify-center group cursor-pointer"
+              key={name}
+            >
+              <div className="flex items-center justify-center space-x-1">
+                <span>
+                  <Icon width={36} height={36} color={color} />
+                </span>
+                <h3 className="font-serif text-2xl text-white truncate font-medium ">
+                  {name}
+                </h3>
+              </div>
+            </motion.div>
+          ))}
         </div>
 
-        <div>
-          <h1></h1>
-        </div>
-        <div className="max-w-2xl lg:max-w-5xl mx-auto">
-          <h2 className="text-black text-2xl md:text-4xl block font-medium text-center mb-8">
-            Rabbitbrain is a free,{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline hover:text-blue-600 align-baseline transition-colors cursor-pointer items-center"
-              href="https://github.com/iyiolacak/rabbitbrain"
-            >
-              <FaGithub className="mr-1 inline" size={32} />
-              open-source
-            </a>
-            &nbsp;platform
-            <span className="text-zinc-400">
-              &nbsp;where you train your mind with <span>AI-generated</span> and
-              &nbsp;
-              <span className="hover:text-green-500 transition-colors cursor-pointer">
-                community-created
+        <section className="py-10">
+          <div>
+            <h1 className="text-9xl text-zinc-200 flex justify-center transition-all">
+              rabbitbrain
+            </h1>
+          </div>
+          <div className="max-w-2xl lg:max-w-5xl mx-auto">
+            <h2 className="text-zinc-800 text-2xl md:text-4xl block font-medium text-center mb-8">
+              is a free,{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline hover:text-blue-600 align-baseline transition-colors cursor-pointer items-center"
+                href="https://github.com/iyiolacak/rabbitbrain"
+              >
+                <FaGithub className="mr-1 inline" size={32} />
+                open-source
+              </a>
+              &nbsp;platform
+              <span className="text-zinc-900">
+                &nbsp;where you train your mind with <span>AI-generated</span>{" "}
+                and &nbsp;
+                <span className="hover:text-green-500 transition-colors cursor-pointer">
+                  community-created
+                </span>
+                &nbsp;challenges. No paywalls, no limits—just smarter thinking,
+                one challenge at a time.
               </span>
-              &nbsp;challenges. No paywalls, no limits—just smarter thinking,
-              one challenge at a time.
-            </span>
-          </h2>
-        </div>
+            </h2>
+          </div>
+        </section>
       </section>
 
       <section className="bg-orange-600 py-16 text-center">
