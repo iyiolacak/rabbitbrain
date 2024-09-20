ba883788304ff6222ab5a6c1aed8c51b1b7ac18d
iyiolacak <iyiolacak0@gmail.com>
2024-09-20 07:58:08 +0300

    fix: unused useMemo removed.

--- src/app/(landing-page)/_components/InfiniteCarousel.tsx
+++ src/app/(landing-page)/_components/InfiniteCarousel.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import conceptsList from "./concepts";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const InfiniteCarousel = () => {
   const [conceptsDisplay, setConceptsDisplay] = useState(
@@ -8,11 +8,6 @@ const InfiniteCarousel = () => {
   );
   const [index, setIndex] = useState(4);
   const [replacementIndex, setReplacementIndex] = useState(0);
-
-  const memoizedConceptsDisplay = useMemo(
-    () => conceptsDisplay,
-    [conceptsDisplay]
-  );
 
   useEffect(() => {
     const updateConceptsDisplay = () => {
@@ -50,23 +45,21 @@ const InfiniteCarousel = () => {
         </motion.div>
       ))}
       {/* Mobile Version: Display only the first element */}
-      {conceptsDisplay
-        .slice(0, 1)
-        .map(({ name, icon: Icon, color }) => (
-          <motion.div
-            className="md:hidden w-full h-16 flex flex-col items-center justify-center group cursor-pointer"
-            key={name}
-          >
-            <div className="flex items-center justify-center space-x-1">
-              <span>
-                <Icon width={36} height={36} color={color} />
-              </span>
-              <h3 className="font-serif text-2xl text-white truncate font-medium ">
-                {name}
-              </h3>
-            </div>
-          </motion.div>
-        ))}
+      {conceptsDisplay.slice(0, 1).map(({ name, icon: Icon, color }) => (
+        <motion.div
+          className="md:hidden w-full h-16 flex flex-col items-center justify-center group cursor-pointer"
+          key={name}
+        >
+          <div className="flex items-center justify-center space-x-1">
+            <span>
+              <Icon width={36} height={36} color={color} />
+            </span>
+            <h3 className="font-serif text-2xl text-white truncate font-medium ">
+              {name}
+            </h3>
+          </div>
+        </motion.div>
+      ))}
     </>
   );
 };
