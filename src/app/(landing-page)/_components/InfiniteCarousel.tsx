a2d678089314189c93726c679a4f62ae5b87c386
iyiolacak <iyiolacak0@gmail.com>
2024-09-20 10:53:31 +0300

    feat: InfiniteCarousel animation initial. - Used `AnimatePresence` and added unique IDs(`Date.now() + Math.random()` (I know it is overkill)) for animate presence to animate each array manipulation

--- src/app/(landing-page)/_components/InfiniteCarousel.tsx
+++ src/app/(landing-page)/_components/InfiniteCarousel.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import conceptsList from "./concepts";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const InfiniteCarousel = () => {
   const [conceptsDisplay, setConceptsDisplay] = useState(
-    conceptsList.slice(0, 4)
+    conceptsList
+      .slice(0, 4)
+      .map((item) => ({ ...item, id: Date.now() + Math.random() }))
   );
   const [index, setIndex] = useState(4);
   const [replacementIndex, setReplacementIndex] = useState(0);
@@ -13,8 +15,11 @@ const InfiniteCarousel = () => {
     const updateConceptsDisplay = () => {
       setConceptsDisplay((prevDisplay) => {
         const newDisplay = [...prevDisplay];
-        newDisplay[replacementIndex % newDisplay.length] =
-          conceptsList[index % conceptsList.length];
+        const newItem = {
+          ...conceptsList[index % conceptsList.length],
+          id: Date.now() + Math.random(),
+        };
+        newDisplay[replacementIndex % newDisplay.length] = newItem;
         return newDisplay;
       });
 
@@ -26,24 +31,35 @@ const InfiniteCarousel = () => {
     return () => clearInterval(interval);
   }, [replacementIndex, index]); // no more dependency on `conceptsDisplay`
 
+  const variants = {
+    enter: { y: 60, opacity: 0 },
+    center: { y: 0, opacity: 100 },
+    exit: { y: -60, opacity: 0 },
+  };
   return (
     <>
       {/* Desktop Version: Display all elements */}
-      {conceptsDisplay.map(({ name, icon: Icon, color }) => (
-        <motion.div
-          className="hidden w-72 h-16 md:flex flex-col items-center group cursor-pointer"
-          key={name}
-        >
-          <div className="flex items-center justify-center space-x-1">
-            <span>
-              <Icon width={48} height={48} color={color} />
-            </span>
-            <h3 className="font-serif text-3xl text-white truncate font-medium ">
-              {name}
-            </h3>
-          </div>
-        </motion.div>
-      ))}
+      <AnimatePresence>
+        {conceptsDisplay.map(({ name, icon: Icon, color, id }) => (
+          <motion.div
+            className="hidden w-72 h-16 md:flex flex-col items-center group cursor-pointer"
+            key={id}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+          >
+            <div className="flex items-center justify-center space-x-1">
+              <span>
+                <Icon width={48} height={48} color={color} />
+              </span>
+              <h3 className="font-serif text-3xl text-white truncate font-medium ">
+                {name}
+              </h3>
+            </div>
+          </motion.div>
+        ))}
+      </AnimatePresence>
       {/* Mobile Version: Display only the first element */}
       {conceptsDisplay.slice(0, 1).map(({ name, icon: Icon, color }) => (
         <motion.div
