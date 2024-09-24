bf939fbccbf1adad35593467d7729be343c26d4b
iyiolacak <iyiolacak0@gmail.com>
2024-09-24 14:17:38 +0300

    FooterCTA updated. "How it works?" Link button added. Kept it responsive.

--- src/app/(landing-page)/_components/FooterCTA.tsx
+++ src/app/(landing-page)/_components/FooterCTA.tsx
@@ -5,23 +5,32 @@ import React from "react";
 const FooterCTA = () => {
   const { navigateTo } = useNavigation();
   return (
-      <section className="bg-orange-600 rounded-[6vh] py-16 text-center">
-        <div className="px-4">
-          <h2 className="text-white font-serif text-3xl md:text-5xl font-medium mb-6">
-            We&apos;re here to learn by doing. Free.
-          </h2>
-          <p className="text-white text-xl mb-8">
-            Join the community and start building your own brain-busting
-            challenges.<br/> Join a community that&apos;s here to learn by doing.
-          </p>
-        </div>
+    <section className="bg-orange-600 rounded-[6vh] py-16 text-center">
+      <div className="px-4">
+        <h2 className="text-white font-serif text-3xl md:text-5xl font-medium mb-6">
+          We&apos;re here to learn by doing. Free.
+        </h2>
+        <p className="text-white text-lg mdtext-xl mb-8">
+          Join the community and start building your own brain-busting
+          challenges.
+          <br />{" "}
+          <span className="text-zinc-200">
+            Join a community that&apos;s here to learn by doing.
+          </span>
+        </p>
+      </div>
+      <div className="flex md:flex-row flex-col w-min mx-auto">
         <Button
           onClick={() => navigateTo("/sign-up")}
-          className="bg-white text-orange-600 font-medium px-8 py-4 text-md tracking-tight rounded-full hover:bg-neutral-300"
+          className="bg-white text-black font-medium px-8 py-4 text-md  rounded-full hover:bg-neutral-300"
         >
           Get Started Now
         </Button>
-      </section>
+        <Button className="text-white" variant={"link"} disabled>
+          How it works?
+        </Button>
+      </div>
+    </section>
   );
 };
 
