be67f0a1374cc37537e80518fcb981b58f8de12b
iyiolacak <iyiolacak0@gmail.com>
2024-10-03 23:34:58 +0300

    feat: fullName check. If user doesn't have a fullName it just reflects on users email.

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -6,9 +6,11 @@ const Home = () => {
   const { user } = useUser();
 
   const email = user?.primaryEmailAddress?.emailAddress || "No email found";
+  const fullName = user?.fullName || "No name found";
+
   return (
     <div className="text-6xl">
-      Currently logged in as {email}
+      Currently logged in as {fullName || email}
     </div>
   );
 };
