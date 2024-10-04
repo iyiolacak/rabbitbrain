ebc2d083184ba94ab36742586649cc6242a3d743
iyiolacak <iyiolacak0@gmail.com>
2024-10-05 00:01:01 +0300

    Remove opacity from transition variants for cleaner animations
    - Updated `transitionVariants` to remove `opacity` attributes.
    - Kept only `x` axis transitions for better alignment with intended animation behavior.

--- src/app/(authentication)/sign-up/page.tsx
+++ src/app/(authentication)/sign-up/page.tsx
@@ -13,9 +13,9 @@ import { useAuthRedirect } from "@/app/hooks/useAuthRedirect";
 import { useUser } from "@clerk/clerk-react";
 
 const transitionVariants = {
-  initial: { opacity: 0, x: 150 },
-  animate: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: -150 },
+  initial: { x: 150 },
+  animate: { x: 0 },
+  exit: { x: -150 },
 };
 
 const SignUpPage = () => {
