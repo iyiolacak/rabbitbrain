c6c5419ce311119867b15e4b283421932d7c10ae
iyiolacak <iyiolacak0@gmail.com>
2024-10-09 20:08:07 +0300

    `success` `success-foreground` defined for further UI usage.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,10 @@ const config: Config = {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
