1adad553eccce12ce3b140a22a384e89df98a5cf
iyiolacak <iyiolacak0@gmail.com>
2024-10-17 07:10:36 +0300

    `subtitle` got commented from `SectionHeader` expecting higher conversion rate.(TO BE TAKEN TO A/B TESTING.)

--- src/app/(authentication)/sign-up/_components/SectionHeader.tsx
+++ src/app/(authentication)/sign-up/_components/SectionHeader.tsx
@@ -13,9 +13,9 @@ const SectionHeader = ({
       <h2 className={`scroll-m-20 text-4xl font-serif font-bold tracking-tight transition-colors first:mt-0 md:mt-5 md:pb-1`}>
         {title}
       </h2>
-      <h3 className="scroll-m-20 text-lg tracking-tight md:mt-1 md:text-xl">
+      {/* <h3 className="scroll-m-20 text-lg tracking-tight md:mt-1 md:text-xl">
         {subtitle}
-      </h3>
+      </h3> */}
     </div>
   );
 };
