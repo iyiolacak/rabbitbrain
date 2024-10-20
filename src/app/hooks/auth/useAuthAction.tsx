716cacffe5753ba1f73fa87fb8fae67968f8a97b
iyiolacak <iyiolacak0@gmail.com>
2024-10-20 14:26:55 +0300

    feat: add custom useAuthAction hook for managing auth states and redirects
    - Implement useAuthAction hook to manage authentication actions based on the current route
    - Handle dynamic updates for "sign-in", "sign-up", and "reset-password" routes
    - Add fallback logic to redirect users to the sign-in page for unmatched routes
    - Ensure proper dependency handling and function memoization with useCallback

--- src/app/hooks/auth/useAuthAction.tsx
+++ src/app/hooks/auth/useAuthAction.tsx
@@ -1,33 +1,46 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import {
-  AuthAction,
-} from "@/app/(authentication)/context/AuthContext";
+import { useEffect, useState, useCallback } from "react";
+import { useRouter } from "next/router";
+import { AuthAction } from "@/app/(authentication)/context/AuthContext";
 import { useAuthStatus } from "@/app/(authentication)/hooks/useAuthStatus";
 
 const useAuthAction = () => {
-
   const { resetAuth } = useAuthStatus();
   const router = useRouter();
-  const { pathname } = router;
 
-  const handleAuthActionFallback = () => {
-    resetAuth();
-    router.push("sign-in")
-    setAuthAction("sign-in")
-    return;
-  }
-
-  // type AuthAction = "sign-up" | "sign-in" | "reset-password"cc 
   const [authAction, setAuthAction] = useState<AuthAction | null>(null);
+
+  const setAction = useCallback(
+    (action: AuthAction) => {
+      resetAuth();
+      setAuthAction(action);
+    },
+    [resetAuth] // Ensuring setAction has the latest reference to resetAuth
+  );
+
+  const handleFallback = useCallback(() => {
+    setAction("sign-in");
+    router.push("/sign-in");
+  }, [setAction, router]);
+
   useEffect(() => {
-    if (!authAction) {
-      handleInvalidToDefaultAuthAction();
-    };
-    if else (pathname)
-  });
+    switch (router.pathname) {
+      case "/sign-in":
+        setAction("sign-in");
+        break;
+      case "/sign-up":
+        setAction("sign-up");
+        break;
+      case "/reset-password":
+        setAction("reset-password");
+        break;
+      default:
+        handleFallback();
+    }
+  }, [router.pathname, setAction, handleFallback]);
+
+  return { authAction };
 };
 
 export default useAuthAction;
