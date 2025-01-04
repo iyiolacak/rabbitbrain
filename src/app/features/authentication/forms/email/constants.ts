e7612d52b95975e82c948aa1ba16f3fbca4d6c1e
iyiolacak <iyiolacak0@gmail.com>
2025-01-04 17:05:13 +0300

    Update constants to include initialAuthStatus. Remove unused initialAuthContext.

--- src/app/features/authentication/forms/email/constants.ts
+++ src/app/features/authentication/forms/email/constants.ts
@@ -1,12 +1,14 @@
-import { AuthContext } from "../../types";
+import { AuthState } from "../../types";
 
 export const BUTTON_ICON_DURATION = 1500; // Duration in ms
 
+export const SUBMISSION_TIMEOUT_MS = 30_000;
+
+
 // --- Initial Context ---
-export const initialAuthContext: AuthContext = {
-  flow: "SignIn", // Default flow
+export const initialAuthStatus: AuthState = {
   method: "Email",
-  stage: "Form",
+  stage: "signIn",
   state: "Idle",
   error: null,
 };
