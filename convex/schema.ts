8ee4e3a05ed58f8b926b896601dfe3f85557d76f
iyiolacak <iyiolacak0@gmail.com>
2025-01-04 15:17:17 +0300

    refactor(database schema): user table is removed.

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,8 +3,5 @@ import { v } from "convex/values";
 import { authTables } from "@convex-dev/auth/server";
 
 export default defineSchema({
-  ...authTables,
-  users: defineTable({
-    name: v.string(),
-  }),
+  ...authTables
 });
